8dcecf6de3c00b2a02c3944e30d1c86feee0adf3
Woogie95 <choisw950928@gmail.com>
2023-08-17 20:02:56 +0900

    Initial commit

---
+++ src/main/java/com/example/bootjaptest/BootJapTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootjaptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJapTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJapTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootjaptest/BootJapTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootjaptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJapTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
